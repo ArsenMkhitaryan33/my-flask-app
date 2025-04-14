f34b6a24f9b4d7ccba828f30186898a752b6542b
Arsen <arsen.mkhitaryan.mkh@gmail.com>
2025-04-14 22:21:43 +0400

    example-2

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     
- return "Hello, aper apres"
- return "hihihihi"
+ return "Hello, aper Bgo"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
