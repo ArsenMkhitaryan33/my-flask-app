6c8d5105139a199d11261577266ce7a9fe467ce2
Arsen <arsen.mkhitaryan.mkh@gmail.com>
2025-04-27 16:40:51 +0400

    change1

--- app.py
+++ app.py
@@ -7,24 +7,17 @@ def home():
     
  return "Hello, aper Hrach)))"
 
-import tkinter as tk
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
-# Create the main window
-root = tk.Tk()
-root.title("Python Button Example")
+from flask import Flask
 
-# Define the action for the button click
-def on_button_click():
-    print("Button clicked!")
+app = Flask(__name__)
 
-# Create a button and add it to the window
-button = tk.Button(root, text="Click Me", command=on_button_click)
-button.pack(pady=20)
-
-# Run the Tkinter event loop
-root.mainloop()
-
-
+@app.route("/")
+def home():
+    
+ return "Hello, aper Hrach)))"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
