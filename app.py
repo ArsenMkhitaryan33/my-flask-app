07d3fe65aa6fb359166b6b515d7cb1c0901a59cd
Arsen <arsen.mkhitaryan.mkh@gmail.com>
2025-04-27 23:23:56 +0400

    change2

--- app.py
+++ app.py
@@ -5,19 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     
- return "Hello Dogs)))"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    
- return "Hello, aper Hrach)))"
+ return "Hello Vahan)))"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
